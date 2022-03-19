f147bc9b17e6a3030bfd5c436d1e048a9c101452
austenc-id <austenc.id@pm.me>
2022-03-19 15:55:19 -0600

    reconfigured and organized data

--- home_app/urls.py
+++ home_app/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 app_name = 'home'
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('django', Django.index, name='django'),
+    path('django/postgres', Django.postgres, name='dj_postgres'),
+    path('django/heroku', Django.heroku, name='dj_heroku'),
 ]
--- home_app/views.py
+++ home_app/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
 def index(Request):
-    return render(Request, 'index.html')
+    return render(Request, 'pages/home.html')
+
+class Django:
+
+    def index(Request):
+        return render(Request, 'pages/django.html')
+
+    def postgres(Request):
+        return render(Request, 'pages/django/postgres.html')
+
+    def heroku(Request):
+        return render(Request, 'pages/django/heroku.html')
