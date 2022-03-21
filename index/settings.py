4b0f1cd15b26683e05e6193dbc430cd5a0465026
austenc-id <austenc.id@pm.me>
2022-03-20 20:24:22 -0600

    debug=True

--- index/settings.py
+++ index/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*+37g1guo2nn+6-9_ll2%u8^9r!gn=76wjgky*l3m6t61v2tjd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'acmf-resume.herokuapp.com'
